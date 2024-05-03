e4548b936551e45085e06592d0924c6ceb4efa5c
Ali Farooq <alifarooq122@gmail.com>
2024-05-03 23:53:14 +0500

    added authorization

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -5,10 +5,12 @@ import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
 export class User {
   @PrimaryGeneratedColumn()
   id: number;
+  @Column({ default: true })
+  admin: boolean;
   @Column()
   email: string;
   @Column()
   password: string;
   @OneToMany(() => Report, (report) => report.user)
-  reports: Report[]
+  reports: Report[];
 }
